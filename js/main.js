6fbeb54741ac6fbe499a3fba27107fa8a8e6211f
Michael-Chacon <michaelchacon02@gmail.com>
2024-01-30 14:41:40 -0500

    feat: :iphone: Menu movile
    Creé el menu para dispositivos moviles, le di funcionalidad con js al icono que despliega el menu
    
    @CampusLands

---
+++ js/main.js
@@ -0,0 +1,5 @@
+let menu = document.querySelector(".navMenu");
+document.querySelector(".btnMenu").addEventListener("click", function () {
+  menu.classList.toggle("active");
+  console.log("vivo");
+});
